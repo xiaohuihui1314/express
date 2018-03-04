379e174ce4a22bc5f96f2d1d59a4e1580d4c4e2e
xiaohuihui1314 <1243817329@qq.com>
2018-03-05 00:40:13 +0800

    修改bug

--- db/controllers/advertisement/advert.js
+++ db/controllers/advertisement/advert.js
@@ -2,17 +2,34 @@ const mongoose = require('mongoose'),
     Advert = mongoose.model('Advert');
 // 查看广告
 exports.getAdvert = (req, res) => {
-    Advert.find({}, (err, docs) => {
-        if (err) {
-            const err = {
-                code: -1,
-                error: "数据异常"
-            };
-            return res.json(err);
-        } else if (docs) {
-            return res.json(docs);
-        }
-    });
+    Advert.find({})
+        .populate("adsenseId")
+        .exec((err, docs) => {
+            if (err) {
+                const err = {
+                    code: -1,
+                    error: "数据异常"
+                };
+                return res.json(err);
+            } else if (docs) {
+                let json = [];
+                docs.forEach(function(item) {
+                    let obj = {};
+                    obj.addressName = item.adsenseId.name;
+                    obj.describe = item.describe;
+                    obj.startTime = item.startTime;
+                    obj.endTime = item.endTime;
+                    obj.link = item.link;
+                    obj.sort = item.sort;
+                    obj.state = item.state;
+                    obj.title = item.title;
+                    obj.url = item.url;
+                    obj.id = item._id;
+                    json.push(obj);
+                });
+                return res.json(json);
+            }
+        });
 };
 // 查看某个广告
 exports.getAdList = (req, res) => {
@@ -59,7 +76,7 @@ exports.deleteAdvert = (req, res) => {
 exports.searchAdvert = (req, res) => {
     // let this_body = req.body;
     Advert
-        .find({ adsenseId: "596f5c69d57b32240c258d48" })
+        .find({ adsenseId: "5a44f95efed2770afc573e29" })
         .populate("adsenseId")
         .exec((err, docs) => {
             if (err) {
